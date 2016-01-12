e977cd8f014c7f0a574114dbfb903d49d7583e64
yitznewton <yitznewton@hotmail.com>
2016-01-11 23:22:58 -0500

    Parse pipe-separated lines

--- lib/entry.rb
+++ lib/entry.rb
@@ -5,44 +5,98 @@ class Entry
   GENDER_FEMALE = 1
 
   def self.from_string(string)
-    new(string)
-  end
-
-  def initialize(string)
-    @string = string
-  end
-
-  def last_name
-    segments[0]
-  end
-
-  def first_name
-    segments[1]
-  end
-
-  def gender
-    case segments[2]
-    when 'Male'
-      GENDER_MALE
-    when 'Female'
-      GENDER_FEMALE
-    else fail ArgumentError
+    if string.include?(',')
+      CommaDelimited.new(string)
+    else
+      PipeDelimited.new(string)
     end
   end
 
-  def birth_date
-    Date.strptime(segments[4], '%m/%d/%Y')
+  class CommaDelimited
+    def initialize(string)
+      @string = string
+    end
+
+    def last_name
+      segments[0]
+    end
+
+    def first_name
+      segments[1]
+    end
+
+    def middle_initial
+      ''
+    end
+
+    def gender
+      case segments[2]
+      when 'Male'
+        GENDER_MALE
+      when 'Female'
+        GENDER_FEMALE
+      else fail ArgumentError
+      end
+    end
+
+    def birth_date
+      Date.strptime(segments[4], '%m/%d/%Y')
+    end
+
+    def favorite_color
+      segments[3]
+    end
+
+    private
+
+    attr_reader :string
+
+    def segments
+      string.split(', ')
+    end
   end
 
-  def favorite_color
-    segments[3]
-  end
+  class PipeDelimited
+    def initialize(string)
+      @string = string
+    end
+    
+    def last_name
+      segments[0]
+    end
 
-  private
+    def first_name
+      segments[1]
+    end
 
-  attr_reader :string
+    def middle_initial
+      segments[2]
+    end
 
-  def segments
-    string.split(', ')
+    def gender
+      case segments[3]
+      when 'F'
+        GENDER_FEMALE
+      when 'M'
+        GENDER_MALE
+      else fail ArgumentError
+      end
+    end
+
+    def favorite_color
+      segments[4]
+    end
+
+    def birth_date
+      Date.strptime(segments[5], '%m-%d-%Y')
+    end
+
+    private
+
+    attr_reader :string
+
+    def segments
+      string.split(' | ')
+    end
   end
 end
--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -11,6 +11,7 @@ describe Entry do
 
       its(:last_name) { is_expected.to eq 'Kelly' }
       its(:first_name) { is_expected.to eq 'Sue' }
+      its(:middle_initial) { is_expected.to eq '' }
       its(:gender) { is_expected.to eq described_class::GENDER_FEMALE }
       its(:favorite_color) { is_expected.to eq 'Pink' }
       its(:birth_date) { is_expected.to eq Date.parse('1959-07-12') }
@@ -19,15 +20,38 @@ describe Entry do
     context 'with male' do
       let(:line) { 'Abercrombie, Neil, Male, Tan, 2/13/1943' }
 
-      its(:last_name) { is_expected.to eq 'Abercrombie' }
-      its(:first_name) { is_expected.to eq 'Neil' }
       its(:gender) { is_expected.to eq described_class::GENDER_MALE }
-      its(:favorite_color) { is_expected.to eq 'Tan' }
-      its(:birth_date) { is_expected.to eq Date.parse('1943-02-13') }
     end
 
     context 'with unknown gender' do
       let(:line) { 'Kelly, Alex, Genderqueer, Pink, 7/12/1959' }
+
+      it 'raises an error' do
+        expect { subject.gender }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  context 'with pipe-delimited data' do
+    context 'with female' do
+      let(:line) { 'Bialik | Vardit | H | F | Orange | 8-2-1657' }
+
+      its(:last_name) { is_expected.to eq 'Bialik' }
+      its(:first_name) { is_expected.to eq 'Vardit' }
+      its(:middle_initial) { is_expected.to eq 'H' }
+      its(:gender) { is_expected.to eq described_class::GENDER_FEMALE }
+      its(:favorite_color) { is_expected.to eq 'Orange' }
+      its(:birth_date) { is_expected.to eq Date.parse('1657-08-02') }
+    end
+
+    context 'with male' do
+      let(:line) { 'Bonk | Radek | S | M | Green | 6-3-1975' }
+
+      its(:gender) { is_expected.to eq described_class::GENDER_MALE }
+    end
+
+    context 'with unknown gender' do
+      let(:line) { 'Bonk | Radek | S | Q | Green | 6-3-1975' }
 
       it 'raises an error' do
         expect { subject.gender }.to raise_error(ArgumentError)
